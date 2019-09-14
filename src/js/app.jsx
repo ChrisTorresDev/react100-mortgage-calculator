4eb77300f941c81e134574db2b1871d21e1200ca
Chris Torres <hwct83@gmail.com>
2019-09-14 16:58:35 -0700

    added constructor element, and updated HTML elements

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,28 +2,57 @@ import React from 'react';
 
 export default class App extends React.Component {
   // your Javascript goes here
+  constructor() {
+    super()
+
+    this.state = {
+      balance: 0,
+      rate: 0,
+    }
+  }
+
+  // handleClick() {
+  //   const calculator = this;
+  // }
+
+  render(){
+    return(
+      <div>
+        content
+      </div>
+    )
+  }
+  
   render() {
     return (
       <div className='container'>
         <div className='row'>
-          <div className='col-md-1'>
-            <input className='balance' type='number'></input>
+          <div>
+            <h1>Mortgage Calculator</h1>
           </div>
-          <div className='col-md-1'>
-            <input className='rate' type='number' step='0.01'></input>
-          </div>
-          <div className='col-md-1'>
-            <select className='term'>
-              <option>15</option>
-              <option>30</option>
-          </select>
-          </div>
-          <div className='col-md-1'>
-            <button className='submit'></button>
-          </div>
-          <div className='col-md-1 output' itemID='output'></div>
+          <form>
+            <div className='form-group'>
+              <label for='balance'>Balance</label>
+              <input className='form-control' name='balance' type='number'></input>
+            </div>
+            <div className='form-group'>
+            <label for='rate'>Rate</label>
+              <input className='form-control' name='rate' type='number' step='0.01'></input>
+            </div>
+            <div className='form-group'>
+              <label for='term'>Term</label>
+              <select className='form-control' name='term'>
+                <option>15</option>
+                <option>30</option>
+              </select>
+            </div>
+            <div className='form-group'>
+              <button name='submit' /* onClick={calculate} */>Calculate</button>
+            </div>
+            <div className='col-md-1 output' id='output'></div>
+          </form>
         </div>
-        {/* your JSX goes here */}
+         {/* your JSX goes here */}
       </div>
     );
   }
