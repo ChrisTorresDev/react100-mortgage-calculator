4318b1311ff8e2fbc23ea00bfb2cb9038685a94e
Chris Torres <hwct83@gmail.com>
2019-09-13 17:46:11 -0700

    added the base HTML elements

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,6 +5,24 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
+        <div className='row'>
+          <div className='col-md-1'>
+            <input className='balance' type='number'></input>
+          </div>
+          <div className='col-md-1'>
+            <input className='rate' type='number' step='0.01'></input>
+          </div>
+          <div className='col-md-1'>
+            <select className='term'>
+              <option>15</option>
+              <option>30</option>
+          </select>
+          </div>
+          <div className='col-md-1'>
+            <button className='submit'></button>
+          </div>
+          <div className='col-md-1 output' itemID='output'></div>
+        </div>
         {/* your JSX goes here */}
       </div>
     );
